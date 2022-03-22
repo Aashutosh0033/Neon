85fccd3f139895d4d6dcae02dadcb83218faf40e
Aashutosh Sarode <a.onelife0033@gmail.com>
2022-03-23 00:13:16 +0530

    added products routes

---
+++ backend/models/productModel.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name : {
+        type: String,
+        required: [true, "Please enter product Name"],
+        trim:true
+
+    },
+
+    description: {
+        type: String,
+        required: [true, "Please enter product Description"]    
+    }, 
+    
+    price : {
+        type: Number,
+        required: [true, "Please enter product Price"],
+        maxLength: [8, "Price cannot exceeed 8 characters"]
+    },
+    rating : {
+        type: Number,
+        default:0
+    },
+
+    images: [
+        {
+            public_id : {
+                type:String,
+                required:true
+            },
+            url: {
+                type:String,
+                required: true
+            }
+        }
+    ],
+    category:{
+        type:String,
+        required: [true, "Please enter product Category"]
+
+
+    },
+
+    stock : {
+        type: Number,
+        required:[true, "Please enter Product stock"],
+        maxLength: [100, "Stock cannot exceed 100 characters"],
+        default: 1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true
+
+            },
+            rating:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+    
+})
+
+module.exports = mongoose.model("Product", productSchema)
