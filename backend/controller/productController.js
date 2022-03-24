7b3f948da8d88eaf7dae1280899a727d7f087254
Aashutosh Sarode <a.onelife0033@gmail.com>
2022-03-24 23:42:13 +0530

    added error handling and search,filter,pagination features

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,39 +1,49 @@
 const { json } = require("express/lib/response");
 const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
+
 
 
 
 
 //create product --admin
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
     const product = await Product.create(req.body);
     res.status(201).json({
         success:true,
         product
     })
 
-}
+});
 
 
 //get all products
-exports.getAllProducts = async (req,res) => {
-    const products = await Product.find()
+exports.getAllProducts = catchAsyncErrors( async (req,res) => {
+
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+
+    const apiFeature = new ApiFeatures(Product.find(),req.query)
+    .search()
+    .filter().pagination(resultPerPage);
+    const products = await apiFeature.query;
     res.status(200).json({
         success:true,
-        products
+        products,
+        productCount
     })
-}
+});
 
 
 //update product --admin
-exports.updateProduct = async (req,res,next) =>{
+exports.updateProduct = catchAsyncErrors (async (req,res,next) =>{
     let product = await Product.findById(req.params.id);
 
     if(!product){
-        return res.status(500).json({
-             success: false,
-             message: "Product not found"
-        });
+        return next(new ErrorHandler("Product not found", 404));
 
     }
 
@@ -42,18 +52,15 @@ exports.updateProduct = async (req,res,next) =>{
         success:true,
         product 
     })
-} 
+} );
 
 
 //Delete product --admin
-exports.deleteProduct = async (req,res,next) =>{
+exports.deleteProduct = catchAsyncErrors (async (req,res,next) =>{
     const product = await Product.findById(req.params.id);
 
     if(!product){
-        return res.status(500).json({
-            success:false,
-            message:"Product not found"
-        })
+        return next(new ErrorHandler("Product not found", 404));
     }
 
     await product.remove();
@@ -62,23 +69,23 @@ exports.deleteProduct = async (req,res,next) =>{
         success:true,
         message:"Product deleted Successfully"
     });
-}
+});
 
 
 
 //Get Product details
-exports.getProductDetails = async(req,res,next) =>{
+exports.getProductDetails = catchAsyncErrors( async(req,res,next) =>{
+
+    
     const product = await Product.findById(req.params.id);
 
     if(!product){
-        return res.status(500).json({
-            success:false,
-            message:"Product not found"
-        })
+        return next(new ErrorHandler("Product not found", 404));
+        
     }
 
     res.status(200).json({
         success:true,
         product 
     })
-}
+});
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,14 @@ const dotenv = require('dotenv');
 
 const connectDatabase = require("./config/database")
 
+
+//Handling Uncaught Exception
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`)
+    console.log(`Shutitng down the server due to uncaught error`)
+    process.exit(1);
+})
+
 // config
 
 dotenv.config({path:"backend/config/config.env"});
@@ -12,6 +20,19 @@ dotenv.config({path:"backend/config/config.env"});
 
 connectDatabase()
 
-app.listen(process.env.PORT , () => {
+const server = app.listen(process.env.PORT , () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
 })
+
+
+//Unhandled Promise rejection
+process.on("unhandledRejection", (err)=>{
+    console.log(`Error: ${err.message} `)
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`)
+
+    server.close(() =>{
+        process.exit(1);
+    });
+
+
+});
