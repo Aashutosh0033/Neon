5477feb30ea4f231934a8ae60be38866e9efe82d
mrcoder991 <udaygirhepunje41@gmail.com>
2022-04-21 11:51:24 +0530

    partner added

--- frontend/src/component/Home/Partner.js
+++ frontend/src/component/Home/Partner.js
@@ -6,8 +6,8 @@ export const Partner = () => {
         <div>
             <h2 className='homeHeading'>Our Partner</h2>
             <div className="container">
-                <img className='partner-logo mx-4' src="https://ik.imagekit.io/onelife0033/SKybox_Png_3xbdiZjKx5.png?ik-sdk-version=javascript-1.4.3&updatedAt=1650290511886" alt="box athelete logo" />
-                <h2 className='align-self-center mx-4'>Sky box athelete</h2>
+                <a href="https://skyboxathlete.com"><img className='partner-logo mx-4' src="https://ik.imagekit.io/onelife0033/SKybox_Png_3xbdiZjKx5.png?ik-sdk-version=javascript-1.4.3&updatedAt=1650290511886" alt="box athelete logo" /></a>
+                <h2 className='align-self-center mx-4'>SkyBox athelete</h2>
             </div>
 
         </div>
